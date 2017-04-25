5c6bb5dc50f28354ad31f923c393303da28bf399
fendertrain <icolbert727@gmail.com>
2017-04-25 08:15:37 +0100

    Add files via upload

---
+++ 45/js/searchlogo.js
@@ -0,0 +1,8 @@
+<script type="application/ld+json">
+{
+  "@context": "http://schema.org",
+  "@type": "Organization",
+  "url": "http://45ismusic.co.uk",
+  "logo": "http://45ismusic.co.uk/img/45.jpg"
+}
+</script>
